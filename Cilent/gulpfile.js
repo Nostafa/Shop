8d31190f105c7ae4b249d0897850acb86c81add9
Mustafa <mostafamok.mm@gmail.com>
2022-03-08 01:42:20 +0200

    start again

--- Cilent/gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const gulpSass = require('gulp-sass');
-const sass = gulpSass(require('sass'));
-gulp.task('scss', async function () {
-  gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('sec/style'));
-});
