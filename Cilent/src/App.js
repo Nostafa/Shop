4348b5bcbfec1080607bba84e768cb80d66491eb
Mustafa <mostafamok.mm@gmail.com>
2022-03-08 11:01:57 +0200

    adding component file

--- Cilent/src/App.js
+++ Cilent/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/index.scss';
+import Home from './pages/Home.jsx';
+import ScrollToTop from './components/ScrollToTop';
+import Categories from './components/Categories';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Promo from './components/Promo';
+import Recommend from './components/Recommend';
+import Services from './components/Services';
+import Choose from './components/Choose';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ScrollToTop />
+      <Navbar />
+      <Home />
+      <Services />
+      <Categories />
+      <Recommend />
+      <Choose />
+      <Products />
+      <Promo />
+      <Footer />
     </div>
   );
 }
-
-export default App;
---
+++ Cilent/src/components/MostSold.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MostSold() {
+  return <div>MostSold</div>;
+}
